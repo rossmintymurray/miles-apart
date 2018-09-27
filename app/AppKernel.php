287bf708ff4aa224321b2bf891e841692438f88e
rossmurray <rossmintymurray@icloud.com>
2018-09-27 23:35:24 +0100

    [CORE] Added backup manager for database backups to server via cronjob

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -38,6 +38,7 @@ class AppKernel extends Kernel
             new Caponica\AmazonMwsBundle\CaponicaAmazonMwsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle(),
+            new BM\BackupManagerBundle\BMBackupManagerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
